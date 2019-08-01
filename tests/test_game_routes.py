eeaa8d06cb04d046f74ceae390e8f103b35ff131
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-08-01 15:42:08 -0300

    Criando put para update de console e adicionando mais coisas nos tests de console e game

--- tests/test_game_routes.py
+++ tests/test_game_routes.py
@@ -4,11 +4,11 @@ from playerstars_routes import get_all_games, post_game
 from playerstars_interactors import SaveGameException
 
 
-@patch('playerstars_routes.game_route.GetAllGamesInteractor.run',
-       MagicMock(return_value='ok'))
-def test_get_all_games():
+@patch('playerstars_routes.game_route.GetAllGamesInteractor.run')
+def test_get_all_games(mock):
     result = get_all_games()
 
+    mock.assert_called_once()
     assert result.body['status'] == 'success'
     assert result.status_code == 200
 
@@ -19,6 +19,7 @@ def test_get_all_games():
 def test_get_all_games_not_found():
     result = get_all_games()
 
+    assert result.body['message'] == 'Nenhum jogo encontrado'
     assert result.body['status'] == 'error'
     assert result.status_code == 404
 
@@ -40,15 +41,17 @@ def make_post_mock_data():
                 }]
     }"""
     data = json.loads(payload)
+    print(data)
     return MagicMock(current_request=MagicMock(json_body=data))
 
 
 # noinspection PyUnusedLocal
 @patch('app.app', make_post_mock_data())
 @patch('playerstars_routes.game_route.PostGameInteractor.run')
-def test_post_user(mock):
+def test_post_game(mock):
     result = post_game()
 
+    mock.assert_called_once()
     assert result.body['status'] == 'success'
     assert result.status_code == 201
 
@@ -56,8 +59,9 @@ def test_post_user(mock):
 @patch('app.app', make_post_mock_data())
 @patch('playerstars_routes.game_route.PostGameInteractor.run',
        MagicMock(side_effect=SaveGameException('oops')))
-def test_post_user_raises():
+def test_post_game_raises():
     result = post_game()
 
+    assert result.body['message'] == 'oops'
     assert result.body['status'] == 'error'
     assert result.status_code == 500
