d989ca0c2b46d30f586984f846b8105e4f3f918b
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-08-13 11:21:09 -0300

    Criando rotas de update para as regioes

--- tests/test_region_country_routes.py
+++ tests/test_region_country_routes.py
@@ -1,15 +1,16 @@
 from unittest.mock import MagicMock, patch
 from playerstars_routes import (
     get_all_region_country, RegionCountryRoute,
-    post_region_country,
+    post_region_country, put_region_country,
     get_region_country_by_id)
 import json
 import pytest
-from playerstars_interactors import SaveRegionCountryException
+from playerstars_interactors import SaveRegionCountryException, \
+    UpdateRegionCountryException
 
 
 @patch('playerstars_routes.region_country_route.'
-       'GetAllCountryRegionsInteractor.run')
+       'GetAllRegionCountriesInteractor.run')
 def test_get_all_region_country(mock):
     result = get_all_region_country()
     mock.assert_called_once()
@@ -19,7 +20,7 @@ def test_get_all_region_country(mock):
 
 # noinspection PyUnusedLocal
 @patch('playerstars_routes.region_country_route.'
-       'GetAllCountryRegionsInteractor.run',
+       'GetAllRegionCountriesInteractor.run',
        MagicMock(return_value=None))
 def test_get_all_region_country_not_found():
     result = get_all_region_country()
@@ -68,6 +69,7 @@ def make_post_mock_data():
 def test_post_region_country(mock):
     result = post_region_country()
 
+    mock.assert_called_once()
     assert result.body['status'] == 'success'
     assert result.status_code == 201
 
@@ -83,22 +85,47 @@ def test_post_region_raises():
     assert result.status_code == 500
 
 
+def make_put_mock_data():
+    payload = """{
+    "entity_id": "id123",
+    "name": "Gold",\
+    "minimum_bet" : 1234,\
+    "countries":["Brasil", "Venezuela", "Cuba"]
+    }"""
+    data = json.loads(payload)
+    return MagicMock(current_request=MagicMock(json_body=data))
+
+
+# noinspection PyUnusedLocal
+@patch('app.app', make_put_mock_data())
+@patch('playerstars_routes.region_country_route.'
+       'PutRegionCountryInteractor.run')
+def test_put_region_country(mock):
+    result = put_region_country()
+
+    mock.assert_called_once()
+    assert result.body['status'] == 'success'
+    assert result.status_code == 200
+
+
+@patch('app.app', make_put_mock_data())
+@patch('playerstars_routes.region_country_route.'
+       'PutRegionCountryInteractor.run',
+       MagicMock(side_effect=UpdateRegionCountryException('oops')))
+def test_put_region_raises():
+    result = put_region_country()
+
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
+
+
 def test_not_implemented():
-    with pytest.raises(NotImplementedError) as exc:
-        RegionCountryRoute().make_put_request({})
-    assert str(exc.value) == 'Update não implementado'
-    with pytest.raises(NotImplementedError) as exc:
-        RegionCountryRoute().update_exception()
-    assert str(exc.value) == 'Update não implementado'
     with pytest.raises(NotImplementedError) as exc:
         RegionCountryRoute().delete_request_model()
     assert str(exc.value) == 'Delete não implementado'
     with pytest.raises(NotImplementedError) as exc:
         RegionCountryRoute().delete_interactor()
     assert str(exc.value) == 'Delete não implementado'
-    with pytest.raises(NotImplementedError) as exc:
-        RegionCountryRoute().put_interactor()
-    assert str(exc.value) == 'Update não implementado'
     with pytest.raises(NotImplementedError) as exc:
         RegionCountryRoute().delete_not_found()
     assert str(exc.value) == 'Delete não implementado'
