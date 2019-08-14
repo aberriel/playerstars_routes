818f78e03f70dd79750ef1f60a793d2912b9f83d
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-08-14 17:09:59 -0300

    Criando rota de match send

--- playerstars_routes/match_list_route.py
+++ playerstars_routes/match_list_route.py
@@ -4,8 +4,10 @@
 from .auth import cors, cupauth
 from chalice import Blueprint
 from playerstars_interactors import GetMatchListRequestModel, \
-    GetMatchListInteractor
-from playerstars_routes.chalice_support import success, not_found
+    GetMatchListInteractor, SendMatchListRequestModel, \
+    SendMatchInterator, SendMatchException
+from playerstars_routes.chalice_support import success, not_found,\
+    server_error, created
 
 bp_match = Blueprint(__name__)
 
@@ -16,6 +18,12 @@ def get_match_list(user_id):
     return MatchListRoute().match_list(user_id)
 
 
+@bp_match.route(
+    '/match-send/', methods=['POST'], cors=cors, authorizer=cupauth)
+def post_match():
+    return MatchListRoute().match_send()
+
+
 class MatchListRoute:
     def match_list(self, user_id):
         request = GetMatchListRequestModel(user_id)
@@ -24,3 +32,14 @@ class MatchListRoute:
         if response:
             return success(response)
         return not_found('Nenhum match encontrado')
+
+    def match_send(self):
+        from app import app
+        data = app.current_request.json_body
+        request = SendMatchListRequestModel(data)
+        interactor = SendMatchInterator(request)
+        try:
+            response = interactor.run()
+        except SendMatchException as e:
+            return server_error(str(e))
+        return created(response)
--- tests/test_match_list_route.py
+++ tests/test_match_list_route.py
@@ -1,5 +1,7 @@
+import json
 from unittest.mock import MagicMock, patch
-from playerstars_routes.match_list_route import get_match_list
+from playerstars_routes.match_list_route import get_match_list, post_match
+from playerstars_interactors import SendMatchException
 
 
 # noinspection PyUnusedLocal
@@ -21,3 +23,34 @@ def test_get_console_not_found():
     assert result.body['message'] == 'Nenhum match encontrado'
     assert result.body['status'] == 'error'
     assert result.status_code == 404
+
+
+def make_post_mock_data():
+    payload = """{
+    "user_id": "userid#123",
+    "challenged_id": "userid#4532"
+    }"""
+    data = json.loads(payload)
+    return MagicMock(current_request=MagicMock(json_body=data))
+
+
+@patch('app.app', make_post_mock_data())
+@patch('playerstars_routes.match_list.SendMatchInterator.run')
+def test_post_console(mock):
+    result = post_match()
+
+    mock.assert_called_once()
+    assert result.body['status'] == 'success'
+    assert result.status_code == 201
+
+
+# noinspection PyUnusedLocal
+@patch('app.app', make_post_mock_data())
+@patch('playerstars_routes.match_list.SendMatchInterator.run',
+       MagicMock(side_effect=SendMatchException('oops')))
+def test_post_console_raises():
+    result = post_match()
+
+    assert result.body['message'] == 'oops'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
