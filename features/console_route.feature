47b087bbbd31f5f06b65624cffffd48e2ba93ff0
Pablo <pablo.moreira@stormsec.com.br>
2019-08-15 16:37:51 -0300

    Criando BDD para rota de console

--- features/console_route.feature
+++ features/console_route.feature
@@ -1,5 +1,6 @@
 Feature: Console integrations tests
     Scenario: Creating a new console
+        Given I set table name as Console
         Given The request has json body
         """
         {
@@ -10,16 +11,16 @@ Feature: Console integrations tests
         }
         """
         When post request is made to /api/console
-        Then The response has status success
-        Then The response has status code 200
-#        Then The saved json has body
-#        """
-#        {
-#            "name": "Super Nintendo",
-#            "logo_path": "/images/ss.png",
-#            "tag_name": "nick#1",
-#            "games" : []
-#        }
-#        """
-#        Then I reset the dynamodb mock
+        Then The response should have status success
+        Then The response should have status_code 201
+        Then The saved json has body
+        """
+        {
+            "name": "Super Selminho",
+            "logo_path": "/images/ss.png",
+            "tag_name": "nick#1",
+            "games" : []
+        }
+        """
+        Then I delete the test entry
 
--- features/steps/basic_steps.py
+++ features/steps/basic_steps.py
@@ -1,17 +1,23 @@
 from app import app
 from behave import *
 import json
-import httpretty
+import boto3
+from playerstars_adapters import ConsoleAdapter
 
 
 class Object(object):
     pass
 
 
-@given("The request has json body")
+@given('The request has json body')
 def json_body(context):
     body = context.text
     context.json_body = json.loads(body)
+
+
+@given('I set table name as {table_name}')
+def json_body(context, table_name):
+    context.table_name = table_name
 
 
 @when('{method} request is made to {url}')
@@ -21,13 +27,51 @@ def json_request(context, method, url):
         app.current_request.json_body = context.json_body
 
     url_method = app.routes.get(url)[method.upper()]
-    function = url_method.view_function()
-    response = function.body
+    response = url_method.view_function()
 
     # response = app.routes.get(url)[method.upper()].view_function().body
     context.response = response
-    print(context.response)
+    context.item_id = context.response.body['data']
+
     try:
         context.response.json = json.loads(context.response)
     except Exception:
         pass
+
+
+@then('The response should have status {status}')
+def json_response_status(context, status):
+    assert context.response.body['status'] == status
+
+
+@then('The response should have status_code {status_code}')
+def json_response_status_code(context, status_code):
+    assert context.response.status_code == int(status_code)
+
+
+convert_string_to_adapter = {
+    'Console': ConsoleAdapter
+}
+
+
+@then('The saved json has body')
+def saved_json(context):
+    body = context.text
+    context.expected_json = json.loads(body)
+
+    response = convert_string_to_adapter[context.table_name]().get_by_id(context.item_id).to_json()
+    del response['entity_id']
+
+    response_string_json = json.dumps(response, sort_keys=True)
+    expected_string_json = json.dumps(context.expected_json, sort_keys=True)
+    assert response_string_json == expected_string_json
+
+
+@then('I delete the test entry')
+def delete_test_entry(context):
+    found = False
+    response = convert_string_to_adapter[context.table_name]().delete(context.item_id)
+    for item in convert_string_to_adapter[context.table_name]().list_all():
+        if response == item.entity_id:
+            found = True
+    assert not found
