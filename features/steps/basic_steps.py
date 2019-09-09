d8314a5210235af8513928709a87fa60e1b5a07f
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-09-09 19:46:02 -0300

    Behaves de player funcionando

--- features/steps/basic_steps.py
+++ features/steps/basic_steps.py
@@ -30,16 +30,6 @@ def saved(context):
     return True if found else False
 
 
-def deleted(context):
-    found = False
-    adapter = context.adapter(context.table_name, context.dynamo_url)
-
-    for item in adapter.list_all():
-        if context.deleted_id == item.entity_id:
-            found = True
-    return True if not found else False
-
-
 @given('I set table name and the adapter class as {table_name}')
 def json_body(context, table_name):
     context.table_name = table_name.lower()
@@ -141,8 +131,8 @@ def saved_json(context):
 
     response_string_json = json.dumps(response, sort_keys=True)
     expected_string_json = json.dumps(context.expected_json, sort_keys=True)
-    print('RESPONSE: ', response_string_json)
-    print('EXPECTED: ', expected_string_json)
+    # print('RESPONSE: ', response_string_json)
+    # print('EXPECTED: ', expected_string_json)
     assert response_string_json == expected_string_json
 
 
@@ -172,6 +162,8 @@ def check_retrieved_json(context):
     context.expected_json = json.loads(body)
     response_string_json = json.dumps(context.response.body['data'], sort_keys=True)
     expected_string_json = json.dumps(context.expected_json, sort_keys=True)
+    print('RESPONSE: ', response_string_json)
+    print('EXPECTED: ', expected_string_json)
     assert response_string_json == expected_string_json
 
 
@@ -181,15 +173,15 @@ def deleted(context):
 
     if hasattr(context, 'list_deleted_id'):
         print("SE EU ENTREI AQUI< TEM ALGUMA COISA MUITO ERRADA")
-        list_all = [x.entity_id for x in context.adapter().list_all()]
+        list_all = [x.entity_id for x in adapter.list_all()]
         for deleted_id in context.list_deleted_id:
             if deleted_id in list_all:
                 found = True
-    print("ALO LIST ALL")
-    for item in adapter.list_all():
-        print("$$$$$$$$: ", item)
-        if context.deleted_id == item.entity_id:
-            found = True
+    else:
+        for item in adapter.list_all():
+            print("$$$$$$$$: ", item)
+            if context.deleted_id == item.entity_id:
+                found = True
     return True if not found else False
 
 
@@ -197,13 +189,18 @@ def deleted(context):
 def check_delete_test_entry(context):
     adapter = context.adapter(context.table_name, context.dynamo_url)
     if hasattr(context, 'dict_list_get_all'):
-        print("SE EU ENTREI AQUI< TEM ALGUMA COISA MUITO ERRADA")
-        for key in context.dict_list_get_all.keys():
-
-            context.deleted_id = adapter.delete(key)
-    print("@@@@@@@@: ", context.item_id)
-    context.deleted_id = adapter.delete(context.item_id)
-    assert deleted(context)
+        context.list_deleted_id = []
+        print("DICT LIST GET ALL: ", context.dict_list_get_all)
+        for dict in context.dict_list_get_all:
+            print("DICT PARA DELETAR: ", dict)
+            context.list_deleted_id.append(adapter.delete(dict['entity_id']))
+        print("DELETADOS: ", context.list_deleted_id)
+        assert deleted(context)
+    else:
+        print(context.item_id)
+        context.deleted_id = adapter.delete(context.item_id)
+        assert deleted(context)
+    assert adapter.list_all() == []
 
 
 @then('The updated entry json has body')
