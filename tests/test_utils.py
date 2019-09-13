e14106a1465231f5e73c683c1e57dd942c9ca367
Pablo <pablo.moreira@stormsec.com.br>
2019-09-13 14:47:05 -0300

    Corrigindo Linter

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -40,18 +40,18 @@ class FakeAdapter:
 
 
 jwt = "eyJraWQiOiI5bisrRW95QnVjUjRoTHRjUnRHeG5yb0YyTkFBT0I0emdxVFlRbXN" \
-          "hWEc4PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI4YWQxNjM1Zi0yMjYzLTRkZG" \
-          "EtODc5YS1iZDI0YjVkOTczMmYiLCJhdWQiOiJhNHUwbG4wMml1bmc1cDcybmtmd" \
-          "HJrczhtIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV2ZW50X2lkIjoiMmRlYTQ1" \
-          "M2QtNmViOS00MzZjLTgzYTUtOTNkYjc4ZjUwMTljIiwidG9rZW5fdXNlIjoiaWQ" \
-          "iLCJhdXRoX3RpbWUiOjE1NjgyMjQ2MjksImlzcyI6Imh0dHBzOlwvXC9jb2duaX" \
-          "RvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX2tPdXFPe" \
-          "GUxYiIsImNvZ25pdG86dXNlcm5hbWUiOiI4YWQxNjM1Zi0yMjYzLTRkZGEtODc5" \
-          "YS1iZDI0YjVkOTczMmYiLCJleHAiOjE1NjgyMzE3OTgsImlhdCI6MTU2ODIyODE" \
-          "5OCwiZW1haWwiOiJ2b3ZvZGVnQGJlLWJyZWF0aHRha2luZy5uZXQifQ.Il5Xmnb" \
-          "JGVCh1j7sSgQ1QlGW6K8oK9SQG1pqybFY8_Yw2n_v021ZfVXCwXhkQW1_i04n3n" \
-          "jBeJMzsyt8hYDyXQFiU6e-3pVyyxkSr6ST3KtHqRcQ9R8kkVM5Y0mXGIyiJ-_CO" \
-          "Z-fdmcpCTajc3DEM-b9okJVv1myIaJITO0b0j57Nu62U6GYnwL9ql-lvF--NYOf" \
-          "yFV9WoybqVJ06TKqks4XjpkCoHP9-pO3-6GqB02leL-mL_U9Jcu-yO6ANVuXn12" \
-          "v8ZCNJjWqNY-LNzdfRShk8GUf92XWxzAu9BuVM9cfKiQL-xznpWMBnuuAY5MjSO" \
-          "_oWDQnH3PZEd_pLdPsLg"
+      "hWEc4PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI4YWQxNjM1Zi0yMjYzLTRkZG" \
+      "EtODc5YS1iZDI0YjVkOTczMmYiLCJhdWQiOiJhNHUwbG4wMml1bmc1cDcybmtmd" \
+      "HJrczhtIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV2ZW50X2lkIjoiMmRlYTQ1" \
+      "M2QtNmViOS00MzZjLTgzYTUtOTNkYjc4ZjUwMTljIiwidG9rZW5fdXNlIjoiaWQ" \
+      "iLCJhdXRoX3RpbWUiOjE1NjgyMjQ2MjksImlzcyI6Imh0dHBzOlwvXC9jb2duaX" \
+      "RvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX2tPdXFPe" \
+      "GUxYiIsImNvZ25pdG86dXNlcm5hbWUiOiI4YWQxNjM1Zi0yMjYzLTRkZGEtODc5" \
+      "YS1iZDI0YjVkOTczMmYiLCJleHAiOjE1NjgyMzE3OTgsImlhdCI6MTU2ODIyODE" \
+      "5OCwiZW1haWwiOiJ2b3ZvZGVnQGJlLWJyZWF0aHRha2luZy5uZXQifQ.Il5Xmnb" \
+      "JGVCh1j7sSgQ1QlGW6K8oK9SQG1pqybFY8_Yw2n_v021ZfVXCwXhkQW1_i04n3n" \
+      "jBeJMzsyt8hYDyXQFiU6e-3pVyyxkSr6ST3KtHqRcQ9R8kkVM5Y0mXGIyiJ-_CO" \
+      "Z-fdmcpCTajc3DEM-b9okJVv1myIaJITO0b0j57Nu62U6GYnwL9ql-lvF--NYOf" \
+      "yFV9WoybqVJ06TKqks4XjpkCoHP9-pO3-6GqB02leL-mL_U9Jcu-yO6ANVuXn12" \
+      "v8ZCNJjWqNY-LNzdfRShk8GUf92XWxzAu9BuVM9cfKiQL-xznpWMBnuuAY5MjSO" \
+      "_oWDQnH3PZEd_pLdPsLg"
