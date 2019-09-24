59c7ad0df602814cfec7ab3c72298979f7c722d2
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-09-24 15:43:28 -0300

    .

--- chalicelib/send_email.py
+++ chalicelib/send_email.py
@@ -12,11 +12,22 @@ bp_email = Blueprint(__name__)
 @bp_email.route('/send', **private_post())
 def post_email():
     data = bp_email.current_request.json_body
-    return post(data)
+    entity_id = get_user_id_from_jwt(bp_email)
+    return post(data, entity_id)
 
 
+<<<<<<< Updated upstream
 def post(json_data):
     request = SendMailRequestModel(json_data)
+=======
+def post(json_data, entity_id):
+    response = get_player_by_id(entity_id)
+    print(dir(response))
+    print(response)
+    if response.body['status'] == 'error':
+        raise BaseException('Player não encontrado')
+    request = SendMailRequestModel(json_data, response.body)
+>>>>>>> Stashed changes
     interactor = SendMailInteractor(request)
     try:
         response = interactor.run()
