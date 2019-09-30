10b1588364bb5463af9e06dee0c9c38dba96f3c9
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-09-30 16:18:33 -0300

    settings updated

--- chalicelib/settings.py
+++ chalicelib/settings.py
@@ -19,3 +19,10 @@ class Settings:
     PAGSEGURO_RETURN_URL = config('PAGSEGURO_RETURN_URL', 'www.google.com.br')
     PURCHASE_OPERATION_TIMEOUT = config('PURCHASE_OPERATION_TIMEOUT', 20)
     PAGSEGURO_SANDBOX_ENABLE = config('PAGSEGURO_SANDBOX_ENABLE', True)
+    PAGSEGURO_EMAIL = config('PAGSEGURO_EMAIL', 'wanderley@stormsec.com.br')
+    PAGSEGURO_TOKEN = config('PAGSEGURO_TOKEN', 'A0ABD34C342A474C8CEB112430FCCBD8')
+    PAGSEGURO_SANDBOX_TOKEN = config('PAGSEGURO_SANDBOX_TOKEN', '8D0B7247DE3B4F22ABBCCCE6AD802C19')
+
+    # "PAGSEGURO_NOTIFICATION_URL": "https://t3hahl6qek.execute-api.us-east-1.amazonaws.com/api/pagseguro/notificacao",
+    # "PAGSEGURO_NOTIFICATIONS_URL": "https://ws.{sandbox}pagseguro.uol.com.br/v3/transactions/notifications/{codigo}?{credenciais}",
+    # "PAGSEGURO_TRANSACTION_URL": "https://ws.{sandbox}pagseguro.uol.com.br/v2/transactions"
