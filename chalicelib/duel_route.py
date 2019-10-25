a67b200c955dccf1af030f93b26d0637008322c4
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-10-25 15:10:50 -0300

    Creating second readme

--- chalicelib/duel_route.py
+++ chalicelib/duel_route.py
@@ -55,7 +55,7 @@ def post_duel():
 
 def create_duel(json_data):
     request = CreateDuelRequestModel(json_data)
-    
+
     interactor = CreateDuelInteractor(
         request=request,
         player_adapter=get_player_adapter(),
