2c329043ed8e626d004dc907f009891334a56343
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-10-30 17:41:29 -0300

    criando listagem de todas as notificações do propio player

--- chalicelib/notification_route.py
+++ chalicelib/notification_route.py
@@ -3,11 +3,13 @@ from playerstars_adapters import (
     NotificationAdapter)
 from chalicelib.settings import Settings
 from playerstars_domain import Notification
-from chalicelib.chalice_support import private_post
-from playerstars_interactors import \
-    PostAppNotificationInteractor, BasicPostRequestModel, SaveEntityException
+from chalicelib.chalice_support import private_post, private_get
+from playerstars_interactors import (
+    PostAppNotificationInteractor, BasicPostRequestModel, SaveEntityException,
+    GetAppNotificationByUserInteractor, GetAppNotificationByUserRequestModel)
 from chalicelib.chalice_support import (
-    server_error, created)
+    server_error, created, success, not_found)
+from chalicelib.utils import get_user_id_from_jwt
 
 bp_notification = Blueprint(__name__)
 
@@ -20,6 +22,8 @@ def get_notification_adapter():
 @bp_notification.route('/', **private_post())
 def post_app_notification():
     data = bp_notification.current_request.json_body
+    entity_id = get_user_id_from_jwt(bp_notification)
+    data.update({'entity_id': entity_id})
     return post(data)
 
 
@@ -32,3 +36,20 @@ def post(json_data):
     except SaveEntityException as e:
         return server_error(str(e))
     return created(response)
+
+
+@bp_notification.route('/', **private_get())
+def get_app_notification():
+    entity_id = get_user_id_from_jwt(bp_notification)
+    return get_by_user(entity_id)
+
+
+def get_by_user(entity_id):
+    adapter = get_notification_adapter()
+    request = GetAppNotificationByUserRequestModel(entity_id)
+    interactor = GetAppNotificationByUserInteractor(request, adapter)
+    response = interactor.run()
+    if response:
+        return success(response)
+    return not_found(f'Nenhuma notificação encontrada para o player id:'
+                     f' {entity_id}')
