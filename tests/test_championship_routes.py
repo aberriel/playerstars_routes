e03e811cd076a372eeec9a3ca726b7ad4a645851
Anselmo Lira <anselmo.lira@stormsec.com.br>
2019-10-30 22:53:32 -0300

    Resolvendo pendências do linter.

--- tests/test_championship_routes.py
+++ tests/test_championship_routes.py
@@ -1,10 +1,7 @@
 from chalicelib import (
     post_accept_invitation
 )
-from playerstars_interactors import (
-    AcceptInvitationException,
-    InvitationNotFoundException
-)
+from playerstars_interactors import AcceptInvitationException
 from tests.test_utils import jwt
 from unittest.mock import MagicMock, patch
 
@@ -12,13 +9,16 @@ import json
 
 
 def make_accept_invitation_mock_data():
-    payload = """{ "invitation_code": "123" }"""
+    payload = """{
+        "invitation_code": "123",
+        "accepted": true
+    }"""
     data = json.loads(payload)
     return MagicMock(current_request=MagicMock(
         json_body=data, headers=dict(AUTHORIZATION=jwt)))
 
 
-@patch('chalicelib.championship_route.bp_championship',
+@patch('chalicelib.championship_route.bp_accept_invitation',
        make_accept_invitation_mock_data())
 @patch('chalicelib.championship_route.AcceptInvitationInteractor.run')
 @patch('boto3.resource')
@@ -29,10 +29,10 @@ def test_post_accept_invitation(boto_client,
     result = post_accept_invitation()
     run.assert_called_once()
     assert result.body['status'] == 'success'
-    assert result.status_code == 201
+    assert result.status_code == 200
 
 
-@patch('chalicelib.championship_route.bp_championship',
+@patch('chalicelib.championship_route.bp_accept_invitation',
        make_accept_invitation_mock_data())
 @patch('chalicelib.championship_route.AcceptInvitationInteractor.run',
        MagicMock(side_effect=AcceptInvitationException('oops')))
