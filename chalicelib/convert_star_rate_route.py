eb87630ea0e6dd711932a5566b4b333c25e90147
Felipe Duarte <felipe.duarte@stormsec.com.br>
2019-11-14 14:06:56 -0300

    creating routes to get and create the convert star rate

---
+++ chalicelib/convert_star_rate_route.py
@@ -0,0 +1,43 @@
+from chalice import Blueprint
+from playerstars_adapters import ConvertStarRateAdapter
+from playerstars_domain import ConvertStarRate
+from chalicelib.chalice_support import (
+    private_get, private_delete, private_put, private_post)
+
+from chalicelib import BasicEntityRoute
+from chalicelib.settings import Settings
+
+bp_convert = Blueprint(__name__)
+
+
+def get_router():
+    adapter = ConvertStarRateAdapter(
+        Settings.CONVERT_STAR_TABLE_NAME, Settings.DYNAMODB_URL)
+    return BasicEntityRoute(adapter, ConvertStarRate, 'console')
+
+
+@bp_convert.route('/', **private_get())
+def get_all_convert_rate():
+    return get_router().get_all()
+
+
+@bp_convert.route('/{entity_id}', **private_get())
+def get_convert_rate_by_id(entity_id):
+    return get_router().get_by_id(entity_id)
+
+
+@bp_convert.route('/', **private_post())
+def post_convert_rate():
+    data = bp_convert.current_request.json_body
+    return get_router().post(data)
+
+
+@bp_convert.route('/{entity_id}', **private_put())
+def put_convert_rate(entity_id):
+    data = bp_convert.current_request.json_body
+    return get_router().put(data)
+
+
+@bp_convert.route('/{entity_id}', **private_delete())
+def delete_convert_rate(entity_id):
+    return get_router().delete(entity_id)
---
+++ tests/test_convert_star_rate.py
@@ -0,0 +1,195 @@
+from chalicelib.convert_star_rate_route import (
+    delete_convert_rate,
+    get_all_convert_rate,
+    get_convert_rate_by_id,
+    post_convert_rate,
+    put_convert_rate
+)
+from playerstars_interactors import SaveEntityException, UpdateEntityException
+from unittest.mock import MagicMock, patch
+
+import json
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicGetAllInteractor.run')
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_get_all_consoles(client, resource, run):
+    result = get_all_convert_rate()
+    run.assert_called_once()
+    assert result.body['status'] == 'success'
+    assert result.status_code == 200
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicGetAllInteractor.run',
+       MagicMock(return_value=None))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_get_all_consoles_not_found(client, resource):
+    result = get_all_convert_rate()
+
+    assert result.body['message'] == 'No console found'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 404
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicGetAllInteractor.run',
+       MagicMock(side_effect=BaseException('oops')))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_get_all_consoles_raises(client, resource):
+    result = get_all_convert_rate()
+
+    assert result.body['message'] == 'oops'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicGetInteractor.run')
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_get_console(client, resource, run):
+    result = get_convert_rate_by_id('id1')
+    run.assert_called_once()
+    assert result.body['status'] == 'success'
+    assert result.status_code == 200
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicGetInteractor.run',
+       MagicMock(return_value=None))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_get_console_not_found(client, resource):
+    result = get_convert_rate_by_id('id1')
+
+    assert result.body['message'] == 'Console not found'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 404
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicGetInteractor.run',
+       MagicMock(side_effect=BaseException('oops')))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_get_console_raises(client, resource):
+    result = get_convert_rate_by_id('id1')
+
+    assert result.body['message'] == 'oops'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
+
+
+def make_post_mock_data():
+    payload = """{
+    "convert_rate": 4
+    }"""
+    data = json.loads(payload)
+    return MagicMock(current_request=MagicMock(json_body=data))
+
+
+def make_put_mock_data():
+    payload = """{
+    "entity_id": "id1",
+    "conver_rate": 5
+    }"""
+    data = json.loads(payload)
+    return MagicMock(current_request=MagicMock(json_body=data))
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.convert_star_rate_route.bp_convert', make_post_mock_data())
+@patch('chalicelib.basic_entity_route.BasicPostInteractor.run')
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_post_console(client, resource, run):
+    result = post_convert_rate()
+
+    run.assert_called_once()
+    assert result.body['status'] == 'success'
+    assert result.status_code == 201
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.convert_star_rate_route.bp_convert', make_post_mock_data())
+@patch('chalicelib.basic_entity_route.BasicPostInteractor.run',
+       MagicMock(side_effect=SaveEntityException('oops')))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_post_console_raises(client, resource):
+    result = post_convert_rate()
+
+    assert result.body['message'] == 'oops'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.convert_star_rate_route.bp_convert', make_put_mock_data())
+@patch('chalicelib.basic_entity_route.BasicPutInteractor.run')
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_put_console(client, resource, mock):
+    result = put_convert_rate('id1')
+
+    mock.assert_called_once()
+    assert result.body['data']
+    assert result.body['status'] == 'success'
+    assert result.status_code == 200
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.convert_star_rate_route.bp_convert', make_put_mock_data())
+@patch('chalicelib.basic_entity_route.BasicPutInteractor.run',
+       MagicMock(side_effect=UpdateEntityException('oops')))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_put_console_raises(client, resource):
+    result = put_convert_rate('id1')
+
+    assert result.body['message'] == 'oops'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicDeleteInteractor.run')
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_delete_console(client, resource, mock):
+    result = delete_convert_rate('id1')
+
+    mock.assert_called_once()
+    assert result.body['status'] == 'success'
+    assert result.status_code == 200
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicDeleteInteractor.run',
+       MagicMock(return_value=None))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_delete_console_not_found(client, resource):
+    result = delete_convert_rate('id1')
+
+    assert result.body['message'] == 'Console not found to be deleted'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 404
+
+
+# noinspection PyUnusedLocal
+@patch('chalicelib.basic_entity_route.BasicDeleteInteractor.run',
+       MagicMock(side_effect=BaseException('oops')))
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_delete_console_raises(client, resource):
+    result = delete_convert_rate('id1')
+
+    assert result.body['message'] == 'oops'
+    assert result.body['status'] == 'error'
+    assert result.status_code == 500
