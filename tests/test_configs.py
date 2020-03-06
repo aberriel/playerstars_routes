67335c71c0b97f7b93ebd5a2c449fbb5b2a20e9d
Anselmo Lira <anselmo.lira@stormsec.com.br>
2020-03-06 17:59:31 -0300

    Alterações neste commit:
    - Atualizando o .chalice/config.json, mapeando as variáveis de ambientes que estão definidas no settings.py e não estão declaradas
    nos ambientes (dev, stg, prd)
    
    - Atualizando o settings.py, arrumando as variáveis declaradas na classe Settings de forma que estejam em ordem alfabética e separadas por
    categiria (nomes de tabelas do DynamoDB em um lado e as outras em outro).
    
    - Desenvolvimento dos testes relacionados à parte de infra da aplicação, isto é:
    	-> verificação da existência do .chalice/config.json
    	-> verificando se todas as variáveis de ambiente que estão no settings/Settings estão definidas no config.json, para os
    	   3 ambientes (dev, stg e prd)
    	-> verificando se o LOG_LEVEL, o nível do log a ser emitido, está definido corretamente no config.json, com as seguintes definições:
    	   INFO para prd e DEBUG para stg e dev
    	-> verificando se todas as variáveis de ambiente relativas a nomes de tabelas do Dynamo estão definidas no config.json, com as seguintes definições:
    
        1. nomes de tabelas definidas globalmente (fora de um stage) não devem possuir nenhum sufixo
        2. nomes de tabelas definidas para stages devem possuir o sufixo correspondente ao stage ('_dev' para dev, '_stg'
           para stg e '_prd' para prd) no nome, obrigatoriamente

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -3,47 +3,51 @@ from os import path
 
 import inspect
 import json
-import os
 
 
 config_file_path = '.chalice/config.json'
 
 
-exclusion_list = ['AWS_DEFAULT_REGION']
+exclusion_list = ['AWS_DEFAULT_REGION', 'DYNAMODB_URL']
 
 
-def get_config_from_json():
+def read_config_json():
     with open(config_file_path) as json_file:
         data = json.load(json_file)
         return data
 
 
 def get_settings_attribute_list():
-    all_attributes = inspect.getmembers(Settings, lambda a:not(inspect.isroutine(a)))
-    public_attributes = [a[0] for a in all_attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
+    all_attributes = inspect.getmembers(
+        Settings, lambda a:not(inspect.isroutine(a)))
+    public_attributes = [a[0] for a in all_attributes
+                         if not(a[0].startswith('__')
+                                and a[0].endswith('__'))]
     return public_attributes
 
 
 def get_settings_variables_table():
-    all_attributes = get_settings_attribute_list()
-    found_table_configs = [x for x in all_attributes if 'TABLE_NAME' in x]
+    config_from_settings = get_settings_attribute_list()
+    found_table_configs = [x for x in config_from_settings
+                           if 'TABLE_NAME' in x]
     return found_table_configs
 
 
 def get_settings_variables_not_table():
-    all_attributes = get_settings_attribute_list()
-    found_table_configs = [x for x in all_attributes if 'TABLE_NAME' not in x]
+    config_from_settings = get_settings_attribute_list()
+    found_table_configs = [x for x in config_from_settings
+                           if 'TABLE_NAME' not in x]
     return found_table_configs
 
 
 def get_environment_variables_from_config():
-    config_content = get_config_from_json()
-    return config_content['environment_variables']
+    config_from_json = read_config_json()
+    return config_from_json['environment_variables']
 
 
 def get_stage_from_config(env):
-    config_content = get_config_from_json()
-    return config_content['stages'][env]
+    config_from_json = read_config_json()
+    return config_from_json['stages'][env]
 
 
 def test_config_file_exists():
@@ -52,38 +56,62 @@ def test_config_file_exists():
 
 
 def test_check_has_stage_dev():
-    config_content = get_config_from_json()
-    assert 'dev' in config_content['stages']
+    config_from_json = read_config_json()
+    assert 'dev' in config_from_json['stages']
 
 
 def test_check_has_stage_stg():
-    config_content = get_config_from_json()
-    assert 'stg' in config_content['stages']
+    config_from_json = read_config_json()
+    assert 'stg' in config_from_json['stages']
 
 
 def test_check_has_stage_prd():
-    config_content = get_config_from_json()
+    config_content = read_config_json()
     assert 'prd' in config_content['stages']
+
+
+def test_has_table_configs_on_environment_variables():
+    tables = get_settings_variables_table()
+    envs_part = get_environment_variables_from_config()
+    for table in tables:
+        assert table in envs_part
+        assert '_dev' not in envs_part[table]
+        assert '_stg' not in envs_part[table]
+        assert '_prd' not in envs_part[table]
+
+
+def test_has_environment_variables_on_environment_variables():
+    envs = get_settings_variables_not_table()
+    envs_part = get_environment_variables_from_config()
+    for env in envs:
+        if env not in exclusion_list:
+            assert env in envs_part
 
 
 def test_has_table_configs_on_dev():
     tables = get_settings_variables_table()
     stage_dev = get_stage_from_config('dev')
     environment_variables = stage_dev['environment_variables']
-    assert(all(table in environment_variables for table in tables))
+    for table in tables:
+        assert table in environment_variables
+        assert '_dev' in environment_variables[table]
 
 
 def test_has_table_configs_on_stg():
     tables = get_settings_variables_table()
-    stage_dev = get_stage_from_config('stg')
-    environment_variables = stage_dev['environment_variables']
-    assert(all(table in environment_variables for table in tables))
+    stage_stg = get_stage_from_config('stg')
+    environment_variables = stage_stg['environment_variables']
+    for table in tables:
+        assert table in environment_variables
+        assert '_stg' in environment_variables[table]
 
 
 def test_has_table_configs_on_prd():
     tables = get_settings_variables_table()
-    environment_variables = get_stage_from_config('prd')
-    assert(all(table in environment_variables for table in tables))
+    stage_prd = get_stage_from_config('prd')
+    for table in tables:
+        assert table in stage_prd
+        assert '_prd' in stage_prd[table]
 
 
 def test_has_environment_variables_on_dev():
@@ -110,3 +138,29 @@ def test_has_environment_variables_on_prd():
     for env in tables:
         if env not in exclusion_list:
             assert env in environment_variables
+
+
+def test_check_log_level():
+    settings_envs = get_settings_variables_not_table()
+    assert 'LOG_LEVEL' in settings_envs
+    json_envs = get_environment_variables_from_config()
+    assert 'LOG_LEVEL' in json_envs
+    assert json_envs['LOG_LEVEL'] == 'INFO'
+
+
+def test_check_log_level_dev():
+    json_dev_envs = get_stage_from_config('dev')['environment_variables']
+    assert 'LOG_LEVEL' in json_dev_envs
+    assert json_dev_envs['LOG_LEVEL'] == 'DEBUG'
+
+
+def test_check_log_level_stg():
+    json_stg_envs = get_stage_from_config('stg')['environment_variables']
+    assert 'LOG_LEVEL' in json_stg_envs
+    assert json_stg_envs['LOG_LEVEL'] == 'DEBUG'
+
+
+def test_check_log_level_prd():
+    json_prd_envs = get_stage_from_config('prd')
+    assert 'LOG_LEVEL' in json_prd_envs
+    assert json_prd_envs['LOG_LEVEL'] == 'INFO'
