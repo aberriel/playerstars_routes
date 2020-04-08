2178c6f31810e545c657a7a9584ebb88665e0400
Felipe Duarte <felipe.duarte@stormsec.com.br>
2020-04-08 14:30:07 -0300

    redeploy

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ app.register_blueprint(bp_leave_team, url_prefix='/team/leave-team')
 app.register_blueprint(bp_match_list, url_prefix='/match-list')
 app.register_blueprint(bp_notification, url_prefix='/notification')
 app.register_blueprint(bp_player, url_prefix='/player')
-
 app.register_blueprint(bp_product, url_prefix='/product')
 app.register_blueprint(bp_purchase, url_prefix='/purchase')
 app.register_blueprint(bp_region_country, url_prefix='/region-country')
