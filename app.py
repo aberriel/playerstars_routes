94338c0303b099fb8273841b93b0de31ed6c0ae2
Felipe Duarte <felipe.duarte@stormsec.com.br>
2020-06-22 17:59:26 -0300

    redeploy

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ app.register_blueprint(bp_admin, url_prefix='/admin')
 app.register_blueprint(bp_cancel_duel, url_prefix='/cancel-duel')
 #app.register_blueprint(bp_championship, url_prefix='/championship')
 app.register_blueprint(bp_console, url_prefix='/console')
+
 app.register_blueprint(bp_console_admin, url_prefix='/admin/console')
 app.register_blueprint(bp_contact_email, url_prefix='/contact-email')
 app.register_blueprint(bp_convert, url_prefix='/convert-rate')
