ffb4f618ef0fa720a451a98be478922004dfeebd
Felipe Duarte <felipe.duarte@stormsec.com.br>
2020-07-09 12:41:34 -0300

    redeploy

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ app.register_blueprint(bp_game, url_prefix='/game')
 app.register_blueprint(bp_game_by_console, url_prefix='/game/console')
 app.register_blueprint(bp_inform_invite_timeout,
                        url_prefix='/duel/inform-invite-timeout')
-
 app.register_blueprint(bp_contact_email, url_prefix='/contact-email')
 app.register_blueprint(bp_invitation_email, url_prefix='/invitation-email')
 app.register_blueprint(bp_welcome_email, url_prefix='/welcome-email')
