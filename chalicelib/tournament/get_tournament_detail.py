7df5ec4d893640002d10a40562db3703d722d8d7
Felipe Duarte <felipe.duarte@stormsec.com.br>
2020-08-03 21:03:26 -0300

    fixing tournament route import and query param requested from my tournaments

--- chalicelib/tournament/get_tournament_detail.py
+++ chalicelib/tournament/get_tournament_detail.py
@@ -3,7 +3,8 @@ from chalicelib.settings import Settings
 from chalicelib.utils import get_user_id_from_jwt
 from chalicelib.tournament.post_tournament import tournament_route
 from playerstars_interactors.tournament.get_tournament_detail import (
-    GetTournamentInteractor, GetTournamentRequestModel, GetTournamentError
+    GetTournamentInteractor, GetTournamentRequestModel, GetTournamentError,
+    GetTournamentAdapters
 )
 from chalicelib.duel_route_adapters import \
     get_player_adapter, get_team_adapter
@@ -27,12 +28,15 @@ def get_tournament_details(entity_id):
         player_id = get_user_id_from_jwt(tournament_route)
         request = GetTournamentRequestModel(
             tournament_id=entity_id, player_id=player_id)
-        interactor = GetTournamentInteractor(
-            request=request,
+        adapters = GetTournamentAdapters(
             player_adapter=get_player_adapter(),
             team_adapter=get_team_adapter(),
             player_tournament_adapter=get_player_tournament_adapter(),
-            team_tournament_adapter=get_team_tournament_adapter(),
+            team_tournament_adapter=get_team_tournament_adapter()
+        )
+        interactor = GetTournamentInteractor(
+            request=request,
+            adapters=adapters,
             tournament_review_time=Settings.TOURNAMENT_REVIEW_TIME)
         response = interactor.run()
         if response:
